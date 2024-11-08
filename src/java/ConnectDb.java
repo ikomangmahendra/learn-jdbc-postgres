a3fddfb9e9ebf27a1c473f26310ba0125c958932
hendra <ikomangmahendra@gmail.com>
2024-11-08 21:21:39 +0800

    change to try resource

--- src/java/ConnectDb.java
+++ src/java/ConnectDb.java
@@ -11,23 +11,24 @@ public class ConnectDb {
         String url = "jdbc:postgresql://localhost:5432/retail";
         String user = "ais";
         String password = "mysecretpassword";
-        Connection connection = DriverManager.getConnection(url, user, password);
-        System.out.println("Connected to database");
 
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from product");
-        while (resultSet.next()) {
-            int id = resultSet.getInt("id");
-            String name = resultSet.getString("name");
-            Integer price = resultSet.getInt("price");
-            String category = resultSet.getString("category");
-            Integer stock = resultSet.getInt("stock");
-            // int price = resultSet.getInt("price");
-            System.out.println(String.format("Product: %d - %s %d %s %d",
-                id, name, price, category, stock));
+        try (var connection = DriverManager.getConnection(url, user, password);) {
+            System.out.println("Connected to database");
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from product");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                Integer price = resultSet.getInt("price");
+                String category = resultSet.getString("category");
+                Integer stock = resultSet.getInt("stock");
+                // int price = resultSet.getInt("price");
+                System.out.println(String.format("Product: %d - %s %d %s %d",
+                    id, name, price, category, stock));
+            }
         }
 
-        connection.close();
         System.out.println("Connection closed");
     }
 }
